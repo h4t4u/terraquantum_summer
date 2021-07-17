18fd78edf5c2cc5e47095d32a355b772ce1c28fe
h4t4u <h4t4u.4ever@gmail.com>
2021-07-17 04:51:52 +0300

    refactoring integer cycles

--- integer_cycles.py
+++ integer_cycles.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-CYCLES_NUM = 3
-NUM = 100
+NUM = 100 # Number of timestamps 
 FILENAMES_FILE = 'filenames'
 CSV_DIR = 'csv'
-timestamps = np.array([1624024010213 + i * 1000 for i in range(100)])
-rates_matrix, currencies = rates_matrix_optimized(timestamps, FILENAMES_FILE, CSV_DIR)
-write_to_csv(rates_matrix, currencies, 0, 'result.csv')
+AMOUNT = 1000000 # Initial amount of money
 
-print(currencies)
-AMOUNT = 1000000
+#
+# This script calculates the most profitable cycles in the integer-deal model, where we can
+# only buy integer amount of each currency. 
+#
+
+timestamps = np.array([1624024010213 + i * 1000 for i in range(NUM)])
+rates_matrix, currencies = rates_matrix_optimized(timestamps, FILENAMES_FILE, CSV_DIR)
+
+# The method calculates the final amount of money after one cycle
 
 def cycle_count_integer(way, t, value):
 	currencies
@@ -31,12 +35,13 @@ def cycle_count_integer(way, t, value):
 			return AMOUNT
 	return value
 
-def recursive_find_cycle_integer(way, value, t):
-    if len(way) > 5:
+# The method recursively finds the most profitable cycle
+
+def recursive_find_cycle_integer(way, value, t): 
+    if len(way) > 5: #cycle length limit
         return
     if len(way) > 1 and way[-1] == way[0]:
         cycle_currencies = np.take(currencies, way)
-        #print(value, cycle_currencies)
         if value >= AMOUNT:
             print('!!!', value, cycle_currencies)
         return
@@ -48,25 +53,27 @@ def recursive_find_cycle_integer(way, value, t):
             continue
         recursive_find_cycle_integer(np.append(way,i), int(value*rates_matrix[t, way[-1], i]*(1-0.2*0.01*0.01)), t)
 
-def check_cycles(cycle):
-	income = np.ones((NUM, CYCLES_NUM))*AMOUNT
+#The method calculates income matrix according to given cycles and timestamps
 
-	file = open('income2', 'wt')
+def count_income_matrix_integer(cycles):
+	income = np.ones((NUM, cycles.shape[0]))*AMOUNT
 
 	for i in range(NUM):
-		for j in range(CYCLES_NUM):
-			income[i,j] = cycle_count_integer(cycle[j], i, AMOUNT)
+		for j in range(cycles.shape[0]):
+			income[i,j] = cycle_count_integer(cycles[j], i, AMOUNT)
 
+	return income
+
+#Plotting the income
+
+def plot_income(income, cycles):
 	fig = plt.figure(figsize=(8,6))
 	ax = fig.add_subplot()
 	ax.set_xlabel('time, s')
 	ax.set_ylabel('income per cycle, base points')
 	ax.set_title('data for 5-node cycle, initial amount ' + str(AMOUNT) + ' ')
-	file.write(str(income))
 
-	for j in range(CYCLES_NUM):
-		plt.plot((timestamps-1624024010213)/1000, (income[:,j]-AMOUNT)/AMOUNT*10000, label = str(cycle[j][:-1]))
+	for j in range(cycles.shape[0]):
+		plt.plot((timestamps-timestamps[0])/1000, (income[:,j]-AMOUNT)/AMOUNT*10000, label = str(cycles[j][:-1]))
 	plt.legend()
 	plt.show()
-
-
