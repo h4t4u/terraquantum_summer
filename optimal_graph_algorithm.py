957a42fbe78358572bdcfe98107e171fbe4b768c
h4t4u <h4t4u.4ever@gmail.com>
2021-08-01 21:40:20 +0300

    hypercube descent refactoring

--- optimal_graph_algorithm.py
+++ optimal_graph_algorithm.py
@@ -1,4 +1,5 @@
 import numpy as np
+from enum import Enum
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import math
@@ -9,93 +10,73 @@ import math
 # good in all cases, where the gradient descent is good, but can be of use in many other situations, too.
 #
 
-# Recursive check of the algorithm
+class Type(Enum):
+	maximum = 'max',
+	minimum = 'min'
 
-def check_minimum_recursively(J, x):
+# Recursive check of the vector, type  = Type.maximum/Type.minimum
+
+def check_extremum_recursively(J, x, type):
 	DIM = J.shape[0]
-	minimum = np.dot(x.T,np.dot(J,x))
+	extr = np.dot(x.T,np.dot(J,x))
+
+	if type != Type.minimum and type != Type.maximum:
+		raise ValueError('Wrong \'type\' variable value')
 
 	for num in range(2**DIM):
 		arr = (((num & (1 << np.arange(DIM)))) > 0).astype(int)*2-1
-		if np.dot(arr.T,np.dot(J,arr)) < minimum:
+		if type == Type.minimum and np.dot(arr.T,np.dot(J,arr)) < extr:
 			print(x, arr)
 			return False
-	return True
 
-def check_maximum_recursively(J, x):
-	DIM = J.shape[0]
-	maximum = np.dot(x.T,np.dot(J,x))
-
-	for num in range(2**DIM):
-		arr = (((num & (1 << np.arange(DIM)))) > 0).astype(int)*2-1
-		if np.dot(arr.T,np.dot(J,arr)) > maximum:
+		if type == Type.maximum and np.dot(arr.T,np.dot(J,arr)) > extr:
 			print(x, arr)
 			return False
 	return True
 
 # The algorithm realisation
 
-def find_bottom_graph(J):
+def find_extremal_graph(J, type):
 	DIM = J.shape[0]
 	x = np.random.sample(DIM)
 	x_scaled = np.sign(x)
-	not_bottom = True
+	not_extremal = True
 
-	while not_bottom:
-		not_bottom = False
-		min_ = np.dot(x_scaled.T,np.dot(J, x_scaled))
+	if type != Type.minimum and type != Type.maximum:
+		raise ValueError('Wrong \'type\' variable value')
+
+	while not_extremal:
+		not_extremal = False
+		extr = np.dot(x_scaled.T,np.dot(J, x_scaled))
 		x_scaled_moved = np.copy(x_scaled)
 
 		for i in range(DIM):
 			x_scaled_moved[i] *= -1
-			min_candidate = np.dot(x_scaled_moved.T,np.dot(J, x_scaled_moved))
+			extr_candidate = np.dot(x_scaled_moved.T,np.dot(J, x_scaled_moved))
 
-			if min_candidate < min_:
-				min_ = min_candidate
+			if type == Type.minimum and extr_candidate < extr:
+				extr = extr_candidate
 				x_scaled = np.copy(x_scaled_moved)
-				not_bottom = True
+				not_extremal = True
 
-			x_scaled_moved[i] *= -1
-	return x_scaled
-
-def find_top_graph(J):
-	DIM = J.shape[0]
-	x = np.random.sample(DIM)
-	x_scaled = np.sign(x*2-1)
-	not_bottom = True
-
-	while not_bottom:
-		not_bottom = False
-		max_ = np.dot(x_scaled.T,np.dot(J, x_scaled))
-		x_scaled_moved = np.copy(x_scaled)
-
-		for i in range(DIM):
-			x_scaled_moved[i] *= -1
-			max_candidate = np.dot(x_scaled_moved.T,np.dot(J, x_scaled_moved))
-
-			if max_candidate > max_:
-				max_ = max_candidate
+			if type == Type.maximum and extr_candidate > extr:
+				extr = extr_candidate
 				x_scaled = np.copy(x_scaled_moved)
-				not_bottom = True
+				not_extremal = True
+
 
 			x_scaled_moved[i] *= -1
 	return x_scaled
 
 def test():
-	steps = np.zeros(18)
-	errors = np.zeros(18)
+	errors = np.zeros(10)
 	samples = 1000
-	for DIM in range(2,20):
+	for DIM in range(2,12):
 		print('DIM', DIM)
 		for i in range(samples):
 			J = np.random.sample((DIM, DIM))
-			x = find_bottom_graph(J)
-			if not check_minimum_recursively(J, x):
+			x = find_extremal_graph(J, Type.minimum)
+			if not check_extremum_recursively(J, x, Type.minimum):
 				errors[DIM-2] += 1
 
-		print('errors',errors)
-		print('mean steps', steps/samples)
-
-
-
-
+		print('errors', errors)
